307db3ecbeb5ec96fc4f10314543b579e7c0e5cf
IRYNA86 <88017665+IRYNA86@users.noreply.github.com>
2022-09-21 22:58:04 +0300

    time 1.36

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import {registerValidation, loginValidation, postCreateValidation} from './validations/AuthAndPost.js';
+import checkAuth from './utils/checkAuth.js';
+// import { register, login, getMe } from "./controllers/UserController.js";
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 mongoose
   .connect(
-    "mongodb+srv://Iryna:mZfWWrg3MQeNVE47@cluster0.kvrgbpo.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://Iryna:mZfWWrg3MQeNVE47@cluster0.kvrgbpo.mongodb.net/blog?retryWrites=true&w=majority"
   )
   .then(() => 
     console.log("DB ok")
@@ -14,28 +18,22 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.post('/auth/login',loginValidation, UserController.login)
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Vasya",
-    },
-    "secret123"
-  );
-  res.json({
-    success: true,
-    token,
-  });
-});
+app.post("/auth/register", registerValidation, UserController.register);
 
-app.listen(4444, (error) => {
-  if (error) {
-    return console.log(error.message);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+// app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+// app.delete('/posts', PostController.remove);
+// app.patch('/posts', PostController.update);
+
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
   }
   console.log("Server OK");
 });
